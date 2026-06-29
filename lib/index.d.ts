ab29771c3374a64c9e0a5b7fcea84b3b204bf6c9
Saurabh0904 <patidarsourabh687@gmail.com>
2026-06-29 17:39:50 +0530

    build run .

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,6 @@
 export { PixelLaunchContainer, type LaunchOrigin, type PixelLaunchContainerProps, } from "./PixelLaunchContainer";
 export { PixelDialog, type PixelDialogButton, type PixelDialogProps, } from "./PixelDialog";
+export { PixelContactDialog, type PixelContactDialogProps, type ContactAction, } from "./PixelContactDialog";
 export { AnimatedBottomSheet, StaggerContext, StaggerItem, type AnimatedBottomSheetProps, type StaggerItemProps, } from "./AnimatedBottomSheet";
 export { DomeFooter, FabMenu, FOOTER_BAR_H, DOME_R, DOME_CX, BTN_R, CUP_RIM_R, type DomeFooterProps, type FabMenuProps, type FabMenuItem, } from "./DomeFooter";
 export { PixelMenuGrid, PixelMenuOverlay, type PixelMenuItem, type PixelMenuSection, type PixelMenuIconRenderer, type PixelMenuGridProps, type PixelMenuOverlayProps, } from "./PixelMenuGrid";
